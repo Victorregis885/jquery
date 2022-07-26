050994218067616e3707c17c649730245cc41add
Victorregis885 <victorregis5002@gmail.com>
2022-07-26 08:25:20 -0300

    commit

---
+++ js/mainipulacao.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('#bto-1-5').click(function(){
+        $('p').addClass('font-1-5');
+    });
+
+    $('#bto-2-0').click(function(){
+        $('p').addClass('font-2-0');
+    });
+
+    $('#bto-2-5').click(function(){
+        $('p').addClass('font-2-5');
+    });
+
+});
